0874d8cf8d0182847e2329db7dd09190ff72889d
Federico Airoldi <airoldibusiness@gmail.com>
2024-03-24 18:44:15 +0100

    food databese aggiunto

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,14 +2,14 @@
 
 export default function Sidebar() {
 	return (
-		<section className="bg-amber-200 text-black p-4 h-screen m-6 rounded">
-			<p>Element 1</p>
-			<p>Element 1</p>
-			<p>Element 1</p>
-			<p>Element 1</p>
-			<p>Element 1</p>
-			<p>Element 1</p>
-			<p>Element 1</p>
+		<section className="bg-amber-200 text-black p-4 h-screen m-6 rounded shadow">
+			<input type="text" placeholder="What do you want to cook?"
+			       className="w-full rounded text-center"
+			/>
+
+			<div>
+
+			</div>
 		</section>
 	)
 }
---
+++ src/database/Food.js
@@ -0,0 +1,139 @@
+export default [
+	{
+		nome: "Spaghetti alla Carbonara (Monoporzione)",
+		immagine: "https://context.reverso.net/traduzione/italiano-inglese/non+valido",
+		descrizione: "Un classico della cucina romana, semplice e saporito.",
+		porzioni: 1,
+		ingredienti: [
+			{
+				nome: "Spaghetti",
+				quantità: 80,
+				unità: "g"
+			},
+			{
+				nome: "Guanciale",
+				quantità: 50,
+				unità: "g"
+			},
+			{
+				nome: "Pecorino romano",
+				quantità: 25,
+				unità: "g"
+			},
+			{
+				nome: "Uova",
+				quantità: 1,
+				unità: "pz"
+			},
+			{
+				nome: "Pepe nero",
+				quantità: "qsp",
+				unità: ""
+			},
+			{
+				nome: "Olio extravergine d'oliva",
+				quantità: "qsp",
+				unità: ""
+			}
+		],
+		istruzioni: [
+			"Cuocere gli spaghetti in acqua bollente salata.",
+			"In una padella soffriggere il guanciale tagliato a cubetti con un filo d'olio.",
+			"Sbattere l'uovo con il pecorino grattugiato.",
+			"Scolare gli spaghetti e saltarli nella padella con il guanciale.",
+			"Togliere dal fuoco e aggiungere il composto di uovo e pecorino, mescolando velocemente.",
+			"Servire con una spolverata di pepe nero."
+		]
+	},
+	{
+		nome: "Insalata Caprese (Monoporzione)",
+		immagine: "https://context.reverso.net/traduzione/italiano-inglese/non+valido",
+		descrizione: "Un fresco e leggero antipasto made in Italy.",
+		porzioni: 1,
+		ingredienti: [
+			{
+				nome: "Pomodoro",
+				quantità: 1,
+				unità: "pz"
+			},
+			{
+				nome: "Mozzarella di bufala",
+				quantità: 125,
+				unità: "g"
+			},
+			{
+				nome: "Basilico fresco",
+				quantità: "qsp",
+				unità: "foglie"
+			},
+			{
+				nome: "Olio extravergine d'oliva",
+				quantità: "qsp",
+				unità: ""
+			},
+			{
+				nome: "Sale",
+				quantità: "qsp",
+				unità: ""
+			},
+			{
+				nome: "Pepe nero",
+				quantità: "qsp",
+				unità: ""
+			}
+		],
+		istruzioni: [
+			"Lavare e tagliare il pomodoro a fette spesse.",
+			"Tagliare la mozzarella a fette spesse e disporla su un piatto.",
+			"Alternare le fette di pomodoro e mozzarella.",
+			"Condire con olio extravergine d'oliva, basilico fresco, sale e pepe nero."
+		]
+	},
+	{
+		nome: "Torta di mele (Monoporzione)",
+		immagine: "https://context.reverso.net/traduzione/italiano-inglese/non+valido",
+		descrizione: "Un dolce classico e intramontabile.",
+		porzioni: 1,
+		ingredienti: [
+			{
+				nome: "Pasta frolla",
+				quantità: 100,
+				unità: "g"
+			},
+			{
+				nome: "Mele",
+				quantità: 1,
+				unità: "pz"
+			},
+			{
+				nome: "Zucchero",
+				quantità: 50,
+				unità: "
+			},
+			{
+				nome: "Burro",
+				quantità: 25,
+				unità: "g"
+			},
+			{
+				nome: "Uova",
+				quantità: 1,
+				unità: "pz"
+			},
+			{
+				nome: "Cannella",
+				quantità: "qsp",
+				unità: ""
+			}
+		],
+		istruzioni: [
+			"Stendere la pasta frolla in una teglia monoporzione.",
+			"Sbucciare e tagliare le mele a fettine.",
+			"Disporre le mele sulla pasta frolla.",
+			"In una ciotola mescolare lo zucchero, il burro fuso e l'uovo.",
+			"Versare il composto sulle mele.",
+			"Cuocere in forno preriscaldato a 180°C per circa 30 minuti.",
+			"Spolverare con cannella a piacere."
+		]
+	}
+];
